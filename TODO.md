0aa9a798f9e76f39452e0c56d132a1d8d3c23571
laaksoharju <mikael.laaksoharju@it.uu.se>
2020-04-25 14:50:57 +0200

    TODO added, starting to fix selection

---
+++ TODO.md
@@ -0,0 +1,20 @@
+# TODO
+
+[ ] Make days selectable
+
+[ ] Make propagation work
+
+[ ] Make flexible, zoomable timeline
+
+[ ] Connect to a DB
+
+[ ] Make scheduling view
+- [ ] For people, additive
+- [ ] For other resources, subtractive
+
+[ ] Add resource reuirements for each row to DB
+
+[ ] Base day capacity on schedules
+
+[ ] Implement actual, historical performance
+- [ ] Recompute future capacity need
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
+    <div class="selection"></div>
+    {{startCoords}} {{endCoords}}
+    <div class="year care">
+      <div class="week" type="text" v-for="week in 52" :key="'n' + week">
+        {{ week }}
+      </div>
+    </div>
     <div class="care capacity" v-for="(item, index) in prognosisStaff" :key="'s' + index">
-      <div>
+      <div class="diagnosis">
         {{item.diagnosis}}
       </div>
       <input class="guesswork" type="text" v-model="item.no_visits">
 <!--       <input class="plan" type="text" v-for="week in 52" :key="week" v-model="item['w_' + week]" :title="prognosisPatCalc[index]['w_' + week]"> -->
-      <div class="plan" type="text" v-for="week in 52" :key="week" :style="{height:(item['w_' + week]*100)+'%'}" :title="prognosisPatCalc[index]['w_' + week]">
-        
+      <div :class="['plan-wrapper', {selected: false && isSelected[week][index]}]" v-for="week in 52" :key="week"
+      @mousedown="setStartCoords(week, index)" @mouseup="setEndCoords()" @mouseover="setSelected(week, index)">
+        <div class="plan" :style="{height:(item['w_' + week]*100)+'%'}" :title="prognosisPatCalc[index]['w_' + week]">
+        </div>
       </div>
       <div class="prev-year">{{prognosisPatCalc[index].plan_min}}</div>
       <input :class="[{'ok': prognosisPatCalc[index].sum > prognosisPatCalc[index].plan_min }, {'warning':prognosisPatCalc[index].sum <= prognosisPatCalc[index].plan_min}]" type="text" v-model="prognosisPatCalc[index].sum">
@@ -22,7 +31,19 @@
 <!- -       <input type="text" v-model="item.plan_max"> - ->
       <input type="text" v-for="week in 52" :key="week" v-model="item['w_' + week]">
       <!- - <input type="text" v-for="week in 52" :key="week" v-model="item['w_' + week]"> - ->
-    </div> -->
+    </div> 
+<div v-selectable="{ 
+         selectedGetter: selectedGetter, 
+         selectedSetter: selectedSetter, 
+         selectingSetter: selectingSetter
+         }" id="app">
+        <div class="selection"></div>
+        <div v-for="(i, item) in items"
+             :class="{ selected: !!selected[i], selecting: !!selecting[i] }"
+           class="selectable" >{{ item }}</div>
+</div>
+
+  -->
   </div>
 </template>
 
@@ -34,7 +55,10 @@ export default {
   data: function () {
     return {
       prognosisPat: [],
-      prognosisStaff: []
+      prognosisStaff: [],
+      isSelected: new Array(52).fill(new Array(31)).fill(null),
+      startCoords: {x: null, y: null},
+      endCoords: {x: null, y: null},
     }
   },
   computed: {
@@ -54,6 +78,27 @@ export default {
       this.prognosisPat = data.prognosisPat;
       this.prognosisStaff = data.prognosisStaff;
     }.bind(this));
+  },
+  methods: {
+    setStartCoords: function (x, y) {
+      this.startCoords = {x: x, y: y}
+      this.selecting = true;
+    },
+    setEndCoords: function () {
+      this.selecting = false;
+    },
+    setSelected: function (x, y) {
+      if (!this.selecting) {
+        return false;
+      }
+      this.endCoords = {x: x, y: y};
+      let coordsMin = {x: Math.min(this.startCoords.x, this.endCoords.x), y: Math.min(this.startCoords.y, this.endCoords.y)}
+      let coordsMax = {x: Math.max(this.startCoords.x, this.endCoords.x), y: Math.max(this.startCoords.y, this.endCoords.y)}
+      if (x >= coordsMin.x && x <= coordsMax.x &&
+          y >= coordsMin.y && y <= coordsMax.y)
+        return true;
+      return false;
+    }
   }
 }
 </script>
@@ -62,13 +107,23 @@ export default {
     display: grid;
     grid-column-gap: 1px;
     margin-bottom: 0.5em;
-    align-items: end;
+    user-select: none;
   }
     .capacity {
     grid-template-columns: 14em 2.5em repeat(52, calc(1.5em)) 2.5em 2.5em;
   }
   .need {
     grid-template-columns: 14em 2.5em 2.5em repeat(auto-fit, calc(1.5em)) 
+  }
+  .plan-wrapper {
+    /*display: flex;*/
+    background-color: white;
+    height:1em;
+    padding:0.2em;
+    align-items: end;
+  }
+  .selected {
+    background-color: yellow;
   }
   .plan {
     background-color: steelblue;
@@ -97,4 +152,17 @@ export default {
     font-family: sans-serif;
     font-size: 9pt;
   }
+  .week {
+    display: inline-block;
+  }
+  .year {
+    display: grid;
+    grid-template-columns: repeat(52, calc(1.5em));
+    margin-left:17em;
+
+  }
+  .diagnosis {
+    user-select: none;
+  }
+
 </style>
