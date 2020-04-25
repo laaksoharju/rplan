9984b46fee0a96e5ec916a0a32fe0da9147f63a6
JorgenMoen <jorgen@jorgen.net>
2020-04-25 22:34:56 +0200

    DB 0.001

---
+++ src/views/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <p>Welcome to Admin.</p>
+    </div>
+   </div>
+</template> 
+
+<script>
+export default {
+  name:"Admin",
+  computed: {
+    username() {
+      // We will see what `params` is shortly
+      return this.$route.params.username
+    }
+  },
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
+  }
+}
+</script>
+<style scoped></style>
