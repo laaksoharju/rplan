4861d1607fed1415f29487a7552bff925caa1c49
laaksoharju <mikael.laaksoharju@it.uu.se>
2020-04-26 20:58:03 +0200

    remove redundant logging

--- dataHandler.js
+++ dataHandler.js
@@ -45,7 +45,6 @@ Data.prototype.initializeTable = function (table) {
   csv({checkType: true, quote:"'"})
     .fromFile("./data/" + table + ".csv")
     .then((jsonObj) => {
-      console.log("JSON object", jsonObj, "done");
       this.data[table] = jsonObj;
     });
 };
